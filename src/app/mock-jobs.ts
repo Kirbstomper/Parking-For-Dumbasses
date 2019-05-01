1d37059f92410a968103d1a3ff72b879771e5160
Kirbstomper <wingedkraby@gmail.com>
2019-05-01 18:59:20 -0400

    Added mock images and changed likes to views

--- src/app/mock-jobs.ts
+++ src/app/mock-jobs.ts
@@ -1,14 +1,9 @@
 import { Job } from './job';
 
 export const MOCK_JOBS: Job[] = [
-  { id: 11, imgPath: 'Mr. Nice', views: 4},
-  { id: 12, imgPath: 'Narco',views: 4 },
-  { id: 13, imgPath: 'Bombasto', views: 4},
-  { id: 14, imgPath: 'Celeritas',views: 4 },
-  { id: 15, imgPath: 'Magneta', views: 4},
-  { id: 16, imgPath: 'RubberMan',views: 4 },
-  { id: 17, imgPath: 'Dynama',views: 4 },
-  { id: 18, imgPath: 'Dr IQ', views: 4},
-  { id: 19, imgPath: 'Magma', views: 4},
-  { id: 20, imgPath: 'Tornado',views: 4 }
+  { id: 11, imgPath: 'src\assets\mockImages\img-00000011.jpg', views: 10},
+  { id: 12, imgPath: 'src\assets\mockImages\img-00000012.jpg',views: 100 },
+  { id: 13, imgPath: 'src\assets\mockImages\img-00000013.jpg', views: 3512},
+  { id: 14, imgPath: 'src\assets\mockImages\img-00000014.jpg',views: 8481 },
+  { id: 15, imgPath: 'src\assets\mockImages\img-00000015.jpg', views: 1}
 ];
