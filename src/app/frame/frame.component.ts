ead23f6f8091e8b0da34623176f1edd8ba0e57c6
Kirbstomper <wingedkraby@gmail.com>
2019-05-01 19:22:36 -0400

    Now showing mock images on homepage

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {Job} from '../job';
+import {MOCK_JOBS} from '../mock-jobs'
 @Component({
   selector: 'app-frame',
   templateUrl: './frame.component.html',
   styleUrls: ['./frame.component.css']
 })
-export class FrameComponent implements OnInit {
 
+export class FrameComponent implements OnInit {
+  jobs = MOCK_JOBS;
+  index = Math.floor(Math.random() * MOCK_JOBS.length)
+  viewing = this.jobs[this.index];
   constructor() { }
- 
+  
   ngOnInit() {
+    
   }
+
+  onClick(){
+    this.index = Math.floor(Math.random() * MOCK_JOBS.length)
+    this.viewing = this.jobs[this.index];
+  }
+
+  
+  
 
 }
